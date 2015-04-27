c2c75ceddd308f6c1aa2ae5e673982183933b8ac
dreamapplehappy <2451731631@qq.com>
2015-04-27 15:22:33 +0800

    添加路由文件

--- cloud/app.js
+++ cloud/app.js
@@ -1,6 +1,7 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
 var app = express();
+var routes = require('cloud/routes');
 
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
@@ -8,13 +9,8 @@ app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
-app.get('/hello', function(req, res) {
-  res.render('index');
-});
-
-app.get('/', function(req, res){
-	res.send("Hello World");
-});
+app.get('/hello', routes.hello);
+app.get('/', routes.index);
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
